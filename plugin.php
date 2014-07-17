3577bb59654196e92c7b86fb480ef055c440412f
Denis Voytyuk <denisvmedia@users.noreply.github.com>
2014-07-18 01:32:10 +0200

    Create plugin.php

---
+++ plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Plugin to include the plugins that are being developed (to use with composer)
+Plugin URI: http://artprima.eu/
+Description: Plugin to include the plugins that are being developed (to use with composer)
+Author: Artprima
+Author URI: http://artprima.eu/
+Version: 1.0
+*/
+
+if (file_exists(__DIR__ . '/../plugins-dev/plugins.php')) {
+    require_once __DIR__ . '/../plugins-dev/plugins.php';
+}
